04320f8dc13805df06522abb0bae8507d812fb7c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-12 14:30:25 +0200

    Added routes

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,11 @@ app.use(express.urlencoded({extended: true}));
 
 const portConfig = require('./config/config');
 const mongooseConfig = require('./config/configMongoose');
+const routes = require('./routes/routes');
 
 
-
+routes(app);
 mongooseConfig(app);
+
 
 app.listen(portConfig.development.PORT, () => console.log(`Server runing on port: ${portConfig.development.PORT}...`))
