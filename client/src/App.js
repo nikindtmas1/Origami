454312d9f68820840b0bbcb8d244a07745b95f01
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-13 18:50:20 +0200

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,57 @@
 
-import './App.css';
+import style from './App.module.css';
 import Hello from './components/Hello';
 import PostList from './components/PostList';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-        <Hello />
+    <div className={style.app}>
+      {/* <header>
+      </header> */}
+      <nav className={style.navigation}>
+            <ul>
+              <li className={style.listItem}><img src="white-origami-bird.png" alt="white-origami" /></li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 1</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 2</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 3</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 4</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 5</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 6</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 7</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 8</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 9</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 10</a>
+                </li>
+                <li className={style.listItem}>
+                    <a href="#">Going to 11</a>
+                </li>
+            </ul>
+        </nav>
+        
         
         <main>
         <PostList />
-      </main>
+        </main>
 
     <footer>
     <p>All rights reserved &copy;</p>
